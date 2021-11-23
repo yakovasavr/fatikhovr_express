069e532d7eb120aed5220e323178b686f915d7a5
renat fatikhov <masha34@gmail.com>
2021-11-23 09:12:50 +0300

    time added

--- testcase/testcase.go
+++ testcase/testcase.go
@@ -54,14 +54,21 @@ func main() {
 
 	// stage2 gathering the Items and processing
 	var i uint64 = 0 // counter for fullfilling a Batch
+	start := time.Now()	// fixing the start point of time
 	for element := range itemchannel{		// element contains 1 Item, received from buffered channel
 		setup = append(setup, element)		// appenfing new value into slice of Items
 		i++
 		// flushing the Batch
-		if (i == input.n){					
+		if (i == input.n){
+			t := time.Now()
+			elapsed := t.Sub(start)
+			if (elapsed < input.d){			// counting the time difference
+				time.Sleep(input.d - elapsed)		// sleeping the gourutine till we get the input.d parameter	
+			}				
 			proccessing(x, ctx, setup)	// processing of Items
 			i = 0
 			setup = setup[:0]			// clearing out a slice
+			start = time.Now()			// resetting a time counter
 		}
 	}
 }
